a979abd4dccf472aeed6329380319343cb68b869
Nicholas <nicholaspriegel@gmail.com>
2024-05-09 14:40:54 -0400

    mvp complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
+  const myInfo ={
+    name: "Nicholas Riegel",
+    profession: "Web Developer",
+  }
+  const mySkills = [
+    {
+      name:'JavaScript',
+      comfort: 6,
+      frontEnd: true,
+      backend: true
+    },
+    {
+      name:'CSS',
+      comfort: 5,
+      frontEnd: true,
+      backend: false
+    },
+    {
+      name:'HTML',
+      comfort: 6,
+      frontEnd: true,
+      backend: false
+    },
+    // ejs, mongoDB, express.js, mongoose, react
+    {
+      name:'EJS',
+      comfort: 5,
+      frontEnd: true,
+      backend: false
+    },
+    {
+      name:'MongoDB',
+      comfort: 5,
+      frontEnd: false,
+      backend: true
+    },
+    {
+      name:'Express.js',
+      comfort: 6,
+      frontEnd: true,
+      backend: false
+    },
+    {
+      name:'Mongoose',
+      comfort: 6,
+      frontEnd: false,
+      backend: true
+    },
+    {
+      name:'React',
+      comfort: 5,
+      frontEnd: true,
+      backend: false
+    },
+    
+  ]
+
+  const frontendSkills = mySkills.map((x, i) => {
+    if (x.frontEnd){
+      return <li key={i}>{x.name} - Comfort Level: {x.comfort}/10</li>
+    }
+  })
+  
+  const backendSkills = mySkills.map((x, i) => {
+    if (x.backend){
+      return <li key={i}>{x.name} - Comfort Level: {x.comfort}/10</li>
+    }
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{myInfo.name}</h1>
+      <h2>{myInfo.profession}</h2>
+      <h3>Skills:</h3>
+      <h4>Frontend:</h4>
+      <ul>
+        {frontendSkills}
+      </ul>
+      <h4>Backend:</h4>
+      <ul>
+        {backendSkills}
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
