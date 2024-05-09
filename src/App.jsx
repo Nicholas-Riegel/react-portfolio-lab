6923c560afc66b0d3d00b788aeec153cccd42d72
Nicholas <nicholaspriegel@gmail.com>
2024-05-09 15:58:55 -0400

    added project and css

--- src/App.jsx
+++ src/App.jsx
@@ -59,32 +59,59 @@ const App = () => {
     
   ]
 
+  const projects = [
+    {
+      title: 'Car Blog', 
+      description: 'This is a fully functional car blog, where users can upload pictures and information about their favorite cars, and other users can leave comments on their cars. Users must sign up and sign in to upload cars or comments. The app exemplifies all the actions of a full-stack app: create, read, update, and delete, and persists user information and data in a database (MongoDB)',
+      image: './images/carBlog.png',
+      url: 'http://18.225.254.20:3002/cars'
+    }
+  ]
+
   const frontendSkills = mySkills.map((x, i) => {
     if (x.frontEnd){
-      return <li key={i}>{x.name} - Comfort Level: {x.comfort}/10</li>
+      return <li key={i}>{x.name} - {x.comfort}/10</li>
     }
   })
   
   const backendSkills = mySkills.map((x, i) => {
     if (x.backend){
-      return <li key={i}>{x.name} - Comfort Level: {x.comfort}/10</li>
+      return <li key={i}>{x.name} - {x.comfort}/10</li>
     }
   })
 
   return (
-    <>
-      <h1>{myInfo.name}</h1>
-      <h2>{myInfo.profession}</h2>
-      <h3>Skills:</h3>
-      <h4>Frontend:</h4>
-      <ul>
-        {frontendSkills}
-      </ul>
-      <h4>Backend:</h4>
-      <ul>
-        {backendSkills}
-      </ul>
-    </>
+    <main>
+      <div id="info-container" className='container'>
+        <h1>{myInfo.name}</h1>
+        <h2>{myInfo.profession}</h2>
+        <h3>Skills:</h3>
+        <h4>Frontend:</h4>
+        <ul>
+          {frontendSkills}
+        </ul>
+        <h4>Backend:</h4>
+        <ul>
+          {backendSkills}
+        </ul>
+      </div>
+      <div id="projects-title-container" className='container'>
+        <h2>Projects</h2>
+      </div>
+      <div id="project-container" className='container'>
+        {projects.map((x, i)=> (
+          <div key={i}>
+            <div className="description-div">
+              <h3>{x.title}</h3>
+              <p>{x.description}</p>
+            </div>
+            <a href={x.url} className="image-div">
+                <img src={x.image} alt="project-image" />
+            </a>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
 
